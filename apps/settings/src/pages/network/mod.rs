ab2fe496280e7183b9f928ab4f65b448d87ac381
divya khanani <divyak@mechasystems.com>
2024-04-05 15:56:47 +0530

    feat(settings-app): network integration started

---
+++ apps/settings/src/pages/network/mod.rs
@@ -0,0 +1,9 @@
+pub mod network_page;
+pub mod add_network_page;
+pub mod ethernet_page;
+pub mod dns_page;
+pub mod network_details_page;
+pub mod manage_networks_page;
+pub mod connect_network_page;
+
+
