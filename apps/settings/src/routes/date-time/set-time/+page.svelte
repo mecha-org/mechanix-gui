0331a9107eb8fc54a12ab8d9cea0ed2cb5bc0053
asadk-mecha <asadk@mechasystems.com>
2024-05-21 15:05:21 +0530

    feat : add remaining pages

---
+++ apps/settings/src/routes/date-time/set-time/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+	import Layout from "$lib/components/layout.svelte";
+	import ListHeading from "$lib/components/list-heading.svelte";
+	import Input from "$lib/components/ui/input/input.svelte";
+
+</script>
+<Layout title="Set time">
+    <ListHeading title="Time set according to the standard time (UST)"/>
+    <div class="w-full grid grid-cols-2 gap-4">
+        <div><Input/></div>
+        <div><Input/></div>
+    </div>
+</Layout>
