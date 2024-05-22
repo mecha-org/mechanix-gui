829b3e03716d69f67304fe471aefe5c1cf936544
asadk-mecha <asadk@mechasystems.com>
2024-05-22 14:55:05 +0530

    feat : add battery page ui

---
+++ apps/settings/src/routes/battery/+page.svelte
@@ -0,0 +1,38 @@
+<script>
+	import Icons from '$lib/components/icons.svelte';
+	import Layout from '$lib/components/layout.svelte';
+	import ListHeading from '$lib/components/list-heading.svelte';
+	import ListItem from '$lib/components/list-item.svelte';
+	import { Progress } from '$lib/components/ui/progress';
+	import { goBack } from '$lib/services/common-services';
+</script>
+
+<Layout title="Battery">
+	<div class="flex flex-col gap-4">
+		<div>
+			<ListHeading title="Battery Percentage" />
+			<div class="bg-midnight-abyss border-twilight-navy rounded-lg border p-6">
+				<Progress value={33} />
+			</div>
+		</div>
+		<ListItem title="Battery performace" isLink href="/battery/battery-performance">
+			<Icons name="right_arrow" height="30px" width="30px" />
+		</ListItem>
+	</div>
+	<footer slot="footer" class="h-full w-full bg-[#05070A73] backdrop-blur-3xl backdrop-filter">
+		<div class="flex h-full w-full flex-row items-center justify-between px-4 py-3">
+			<button
+				class="bg-ash-gray flex h-[48px] w-[48px] rotate-180 items-center justify-center rounded-lg p-2 text-[#FAFBFC]"
+				on:click={goBack}
+			>
+				<Icons name="right_arrow" width="32" height="32" />
+			</button>
+			<button
+				class="bg-ash-gray flex h-[48px] w-[48px] items-center justify-center rounded-lg p-2 text-[#FAFBFC]"
+				on:click={goBack}
+			>
+				<Icons name="tick" width="32" height="32" />
+			</button>
+		</div>
+	</footer>
+</Layout>
---
+++ apps/settings/src/routes/battery/battery-performance/+page.svelte
@@ -0,0 +1,51 @@
+<script>
+	import Icons from '$lib/components/icons.svelte';
+	import Layout from '$lib/components/layout.svelte';
+	import ListItem from '$lib/components/list-item.svelte';
+	import { goBack } from '$lib/services/common-services';
+	let batteryModeArr = ['Low', 'Balanced', 'High'];
+	let selectedMode = 'Low';
+</script>
+
+<Layout title="Performance mode">
+	<div class="flex flex-col gap-4">
+		<div class="flex flex-col gap-4">
+			{#each batteryModeArr as mode, index}
+				<button
+					on:click={() => {
+						selectedMode = mode;
+					}}
+					><ListItem isSelected={selectedMode == mode} title={mode}>
+						{#if mode == selectedMode}
+							<Icons name="blue_checked" height="30px" width="30px" />
+						{:else}
+							<Icons name="empty_ring" height="30px" width="30px" />
+						{/if}
+					</ListItem></button
+				>
+			{/each}
+		</div>
+		{#if selectedMode == 'High'}
+			<p class="text-misty-slate">
+				Higher performance will use battery faster and increase the temperature of the device
+				significantly. Check ambient temperature before proceeding.
+			</p>
+		{/if}
+	</div>
+	<footer slot="footer" class="h-full w-full bg-[#05070A73] backdrop-blur-3xl backdrop-filter">
+		<div class="flex h-full w-full flex-row items-center justify-between px-4 py-3">
+			<button
+				class="bg-ash-gray flex h-[48px] w-[48px] rotate-180 items-center justify-center rounded-lg p-2 text-[#FAFBFC]"
+				on:click={goBack}
+			>
+				<Icons name="right_arrow" width="32" height="32" />
+			</button>
+			<button
+				class="bg-ash-gray flex h-[48px] w-[48px] items-center justify-center rounded-lg p-2 text-[#FAFBFC]"
+				on:click={goBack}
+			>
+				<Icons name="tick" width="32" height="32" />
+			</button>
+		</div>
+	</footer>
+</Layout>
