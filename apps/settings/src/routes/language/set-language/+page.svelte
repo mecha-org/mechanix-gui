2a97dc64bb127048235b8d370b3a8acb42149f30
swetar-mecha <swetar@mechasystems.com>
2024-06-13 12:09:03 +0530

    misc: ui update

--- apps/settings/src/routes/language/set-language/+page.svelte
+++ apps/settings/src/routes/language/set-language/+page.svelte
@@ -1,7 +1,10 @@
-<script>
+<script lang="ts">
 	import Icons from '$lib/components/icons.svelte';
 	import Layout from '$lib/components/layout.svelte';
 	import { goBack } from '$lib/services/common-services';
+	const submitHandler = () => {
+		goBack();
+	};
 </script>
 
 <Layout title="Select Language">
@@ -35,13 +38,19 @@
 		</div>
 	</div>
 	<footer slot="footer" class="h-full w-full bg-[#05070A73] backdrop-blur-3xl backdrop-filter">
-		<div class="flex h-full w-full flex-row items-center justify-between px-4 py-3">
-			<button
-				class="flex h-[48px] w-[48px] rotate-180 items-center justify-center rounded-lg bg-ash-gray p-2 text-[#FAFBFC]"
-				on:click={goBack}
-			>
-				<Icons name="right_arrow" width="32" height="32" />
-			</button>
-		</div>
+			<div class="flex h-full w-full flex-row items-center justify-between px-4 py-3">
+				<button
+					class="bg-ash-gray flex h-[48px] w-[48px] rotate-180 items-center justify-center rounded-lg p-2 text-[#FAFBFC]"
+					on:click={goBack}
+				>
+					<Icons name="right_arrow" width="32" height="32" />
+				</button>
+				<button
+					class="bg-ash-gray hover:bg-[#474749]/80 flex h-[48px] w-[48px] items-center justify-center rounded-lg p-2 text-[#FAFBFC]"
+					on:click={submitHandler}
+				>
+					<Icons name="tick" width="32" height="32" />
+				</button>
+			</div>
 	</footer>
 </Layout>
