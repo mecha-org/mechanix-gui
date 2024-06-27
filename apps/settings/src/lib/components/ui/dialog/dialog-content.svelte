940b6343dff281c29b73bdb3e3fddcd4b0d92563
swetar-mecha <swetar@mechasystems.com>
2024-06-27 14:19:50 +0530

    chore: build fix in dialog component

--- apps/settings/src/lib/components/ui/dialog/dialog-content.svelte
+++ apps/settings/src/lib/components/ui/dialog/dialog-content.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { Dialog as DialogPrimitive } from "bits-ui";
-	import X from "lucide-svelte/icons/x";
+	// import X from "lucide-svelte/icons/x";
 	import * as Dialog from "./index.js";
 	import { cn, flyAndScale } from "$lib/utils.js";
 
