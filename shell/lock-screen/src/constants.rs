24caa16b20349235b4dd5d1a67557dca1a4a71c9
sweta <swetar@mechasystems.com>
2024-07-19 11:58:23 +0530

    chore: config default values for lock-screen

---
+++ shell/lock-screen/src/constants.rs
@@ -0,0 +1,10 @@
+pub const SYSTEM_MECHANIX_LOCK_SCREEN_PATH : &str  = "/usr/share/mechanix/lock-screen/icons/";
+
+pub const HOME_ICON : &str = "home.svg";
+pub const BACKSPACE_ICON : &str = "backspace.svg";
+pub const BACK_ICON : &str = "back.svg";
+pub const LOCK_ICON : &str = "lock.svg";
+pub const UNLOCK_ICON : &str = "unlock.svg";
+pub const BACKGROUND_ICON : &str = "mecha-background-color.png";
+
+pub const PASSWORD_LENGTH : usize = 4;
