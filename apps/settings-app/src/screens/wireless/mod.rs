902fc1c940ebe7d2b1ccc8b7169594b23f4a3332
swetar-mecha <swetar@mechasystems.com>
2024-12-06 16:06:05 +0530

    remove old wireless module

--- apps/settings-app/src/screens/wireless/mod.rs
+++
@@ -1,8 +0,0 @@
-pub mod available_networks;
-pub mod component;
-pub mod connect_network_enter_code;
-pub mod errors;
-pub mod handler;
-pub mod manage_networks;
-pub mod network_details_screen;
-pub mod network_screen;
