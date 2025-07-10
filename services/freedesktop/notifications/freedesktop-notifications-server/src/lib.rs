319282e9375b6e3e8c2515a5462b4760f5ff88a6
sandptel <sandeeppatel626735@gmail.com>
2025-07-10 18:46:42 +0530

    feat: flow description added

--- services/freedesktop/notifications/freedesktop-notifications-server/src/lib.rs
+++ services/freedesktop/notifications/freedesktop-notifications-server/src/lib.rs
@@ -2,3 +2,7 @@ pub mod errors;
 pub mod interfaces;
 pub mod proxies;
 pub mod notification;
+
+// Flow
+
+// {Applications by default calls notify/close method on } --> org.freedesktop.Notifications (with Notification Fields)--> FreeDesktopNotificaitonManager (Convert it to Notification Struct) -> Signal ([Notify/Close]) -> org.mechanix.NotificationManager --> Signal <--listerning from UI side --> (Mechanix GUI) --> Show/Remove --> GUI with buttons {Buttons Description provided in Notification Struct Hints} --> yes (GUI  calls method on org.mecahnix.NotifiactionManager for the Action Equivalent)--> MecahnixNotificationService (Signal Emmiter : FreeDesktop) --> Action Signal Emmited via org.freedesktop.Notification -> { <-- signal listner on org.freedesktop.Notification--> Application}.
