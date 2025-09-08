163d59d85d2e0b7c94f5332db919c5641016d049
akshayr-mecha <akshayr@mechasystems.com>
2025-09-08 12:37:20 +0530

    feat(utils): adds desktop apps plugin

--- shell/crates/utils/src/lib.rs
+++ shell/crates/utils/src/lib.rs
@@ -1,5 +1,7 @@
-mod font_assets_plugin;
+mod desktop_apps;
+mod font_assets;
 
 pub mod prelude {
-    pub use crate::font_assets_plugin::{FontAssetsPlugin, fonts_loaded};
+    pub use crate::desktop_apps::{DesktopApp, DesktopApps, DesktopAppsPlugin};
+    pub use crate::font_assets::{FontAssetsPlugin, fonts_loaded};
 }
