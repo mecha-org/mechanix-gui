a55dfbe1cc374cbfb32b9bb0dfba72fc03ca0388
akshayr-mecha <akshayr@mechasystems.com>
2025-11-21 14:24:14 +0530

    chore(launcher): removes unused code

--- shell/crates/settings_drawer/src/ui/mod.rs
+++ shell/crates/settings_drawer/src/ui/mod.rs
@@ -209,28 +209,7 @@ impl Render for SettingsDrawer {
                                             );
                                             cx.notify();
                                         }),
-                                    ), // .on_click(cx.listener(|this, _, window, cx| {
-                                       //     let is_open = this.position == 0.;
-                                       //     let mut regions = Vec::new();
-                                       //     if is_open {
-                                       //         this.position = Self::closed_pos();
-                                       //         regions.push(Bounds {
-                                       //             origin: point(
-                                       //                 px(APP_SIZE.0 - NAVBAR_SIZE.0),
-                                       //                 px(Self::closed_pos()),
-                                       //             ),
-                                       //             size: size(px(APP_SIZE.0), px(APP_SIZE.1)),
-                                       //         });
-                                       //     } else {
-                                       //         this.position = 0.;
-                                       //         regions.push(Bounds {
-                                       //             origin: point(px(0.), px(0.)),
-                                       //             size: size(px(APP_SIZE.0), px(APP_SIZE.1)),
-                                       //         });
-                                       //     }
-                                       //     window.set_input_regions(Some(regions));
-                                       //     cx.notify();
-                                       // })),
+                                    ),
                             ),
                     )
                     .child(self.drawer_items(cx)),
