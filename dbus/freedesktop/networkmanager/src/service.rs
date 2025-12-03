96e89eb716b565f01d5bb8f0e493a21321c4b294
Minesh Purohit <mineshp@mechasystems.com>
2025-12-03 19:05:14 +0530

    feat(network):access point info added to payload

--- dbus/freedesktop/networkmanager/src/service.rs
+++ dbus/freedesktop/networkmanager/src/service.rs
@@ -9,12 +9,15 @@ use crate::proxies::NetworkManagerProxy;
 use anyhow::Result;
 use futures::executor::ThreadPool;
 use futures::{FutureExt, StreamExt};
+use futures::channel::mpsc;
 use log::{debug, error, info};
-use std::sync::{mpsc, LazyLock};
+use std::sync::LazyLock;
 use zbus::Connection;
 
 static THREAD_POOL: LazyLock<ThreadPool> =
     LazyLock::new(|| ThreadPool::new().expect("Failed to build pool"));
+
+const CHANNEL_SIZE: usize = 10;
 
 /// A service wrapper for interacting with a NetworkManager implementation.
 ///
@@ -211,14 +214,14 @@ impl NetworkManagerService {
 
     pub async fn stream_device_events(&self) -> mpsc::Receiver<NMState> {
         let proxy = self.proxy.clone();
-        let (sender, receiver) = mpsc::channel();
+        let (mut sender, receiver) = mpsc::channel(CHANNEL_SIZE);
 
         THREAD_POOL.spawn_ok(async move {
             match proxy.stream_device_events().await {
                 Ok(mut stream) => {
                     while let Some(event) = stream.next().await {
                         if let Ok(state) = event.get().await {
-                            if let Err(e) = sender.send(NMState::from(state)) {
+                            if let Err(e) = sender.try_send(NMState::from(state)) {
                                 error!("failed to send device event to receiver: {}", e);
                                 continue;
                             }
@@ -237,7 +240,7 @@ impl NetworkManagerService {
         &self,
     ) -> mpsc::Receiver<Result<AccessPointEvent, NetworkManagerError>> {
         let proxy = self.proxy.clone();
-        let (sender, receiver) = mpsc::channel();
+        let (mut sender, receiver) = mpsc::channel(CHANNEL_SIZE);
 
         THREAD_POOL.spawn_ok(async move {
             let (mut access_point_added_stream, mut access_point_removed_stream) =
@@ -271,14 +274,26 @@ impl NetworkManagerService {
                                 }
                             };
 
+                            let protected_or_open = raw_access_point_info
+                            .nm80211_flags()
+                            .contains(NM80211ApFlags::PRIVACY)
+                            .then(|| "Protected".to_string()).unwrap_or("Open".to_string());
+
                             let access_point_event_info = AccessPointEvent {
                                 access_point_path,
                                 event_type: EventType::Added,
-                                raw_access_point_info: Some(raw_access_point_info),
+                                wireless_network_info: Some(WirelessNetworkInfo {
+                                    ssid: raw_access_point_info.ssid,
+                                    signal_strength: raw_access_point_info.strength,
+                                    security: protected_or_open,
+                                    hw_address: raw_access_point_info.hw_address,
+                                    is_active: raw_access_point_info.is_active,
+                                    is_known: raw_access_point_info.is_known,
+                                }),
                                 ..Default::default()
                             };
 
-                            if sender.send(Ok(access_point_event_info)).is_err() {
+                            if sender.try_send(Ok(access_point_event_info)).is_err() {
                                 error!("failed to send access point added: receiver dropped");
                                 continue; // Receiver dropped
                             }
@@ -301,11 +316,11 @@ impl NetworkManagerService {
                             let access_point_event_info = AccessPointEvent {
                                 access_point_path,
                                 event_type: EventType::Removed,
-                                raw_access_point_info: None,
+                                wireless_network_info: None,
                                 ..Default::default()
                             };
 
-                            if sender.send(Ok(access_point_event_info)).is_err() {
+                            if sender.try_send(Ok(access_point_event_info)).is_err() {
                                 error!("failed to send access point added: receiver dropped");
                                 continue; // Receiver dropped
                             }
@@ -320,14 +335,14 @@ impl NetworkManagerService {
     }
     pub async fn stream_wireless_enabled_status(&self) -> mpsc::Receiver<bool> {
         let proxy = self.proxy.clone();
-        let (sender, receiver) = mpsc::channel();
+        let (mut sender, receiver) = mpsc::channel(CHANNEL_SIZE);
         THREAD_POOL.spawn_ok(async move {
             match proxy.stream_wireless_enabled_status().await {
                 Ok(mut stream) => {
                     while let Some(event) = stream.next().await {
                         if let Ok(state) = event.get().await {
                             info!("state updated: {}", state);
-                            if let Err(e) = sender.send(state) {
+                            if let Err(e) = sender.try_send(state) {
                                 error!("failed to send device event to receiver: {}", e);
                                 continue;
                             }
@@ -345,7 +360,7 @@ impl NetworkManagerService {
         println!("service-action:: streaming active network strength");
         info!("service-action:: streaming active network strength");
         let proxy = self.proxy.clone();
-        let (sender, receiver) = mpsc::channel();
+        let (mut sender, receiver) = mpsc::channel(CHANNEL_SIZE);
 
         THREAD_POOL.spawn_ok(async move {
             match proxy.stream_wireless_network_strength().await {
@@ -353,7 +368,7 @@ impl NetworkManagerService {
                     while let Some(event) = stream.next().await {
                         if let Ok(state) = event.get().await {
                             info!("network strength updated: {}", state);
-                            if let Err(e) = sender.send(state) {
+                            if let Err(e) = sender.try_send(state) {
                                 error!("failed to send strength event to receiver: {}", e);
                                 continue;
                             }
