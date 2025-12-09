6d1005d337e91487114b380ee69d49199360577a
Minesh Purohit <mineshp@mechasystems.com>
2025-12-09 11:57:12 +0530

    refactor(mxconf):client example update for ease to configure

--- dbus/mechanix/mxconf/examples/client.rs
+++ dbus/mechanix/mxconf/examples/client.rs
@@ -11,23 +11,27 @@ use mxconf_dbus::{get_setting, watch_setting};
 #[tokio::main]
 async fn main() -> Result<()> {
     // Example: query a config key from the D-Bus service.
-    let key = "org.mechanix.keyboard.general.layout";
-    let schema = "org.mechanix.keyboard";
+    // To register your schema, place a new TOML file in `/usr/share/mxconf/schemas`.
+    // For more information, see the README.md file:
+    // https://github.com/mecha-org/mechanix-gui/tree/pre-release/services/conf#-configuration-schema
+    let registered_schema = "org.mechanix.launcher";
+    let key_to_watch = "org.mechanix.launcher.theme.color";
+
     // Call your get_setting function.
-    match watch_setting(schema, None).await {
+    match watch_setting(registered_schema, Some(key_to_watch.to_string())).await {
         Ok(mut stream) => {
-            println!("Watching for changes to {}...", key);
+            println!("Watching for changes to {}...", key_to_watch);
             tokio::spawn(async move {
                 while let Some(signal) = stream.next().await {
                     if let Ok((_schema, signal_key, value)) = signal.body::<(String, String, String)>() {
                         println!("Received change signal for key: {}", signal_key);
                     } else {
-                        println!("Failed to parse signal body for key: {:?}", key);
+                        println!("Failed to parse signal body for key: {:?}", key_to_watch);
                     }
                 }
             });
         }
-        Err(e) => println!("Failed to watch for changes to {}: {}", key, e),
+        Err(e) => println!("Failed to watch for changes to {}: {}", key_to_watch, e),
     }
 
 
