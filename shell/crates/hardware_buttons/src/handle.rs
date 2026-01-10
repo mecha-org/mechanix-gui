f7ae20a1913cdbafe9deb0fb428c2134a4c8ed71
sandptel <sandeeppatel626735@gmail.com>
2026-01-10 17:08:57 +0530

    refactor(hardware_buttons/handle.rs): println -> info

--- shell/crates/hardware_buttons/src/handle.rs
+++ shell/crates/hardware_buttons/src/handle.rs
@@ -1,97 +1,98 @@
 use dispatcher::Message;
 use hw_buttons::{Key, KeyEvent};
+use log::info;
 
 pub fn build_message_for_event(event: KeyEvent) -> Option<Message> {
-    println!("hardware_buttons: received hardware button event: {event:?}");
+    info!("hardware_buttons: received hardware button event: {event:?}");
 
     let mut message: Option<Message> = None;
 
     match event {
         KeyEvent::Pressed(Key::Power) => {
-            println!("hardware_buttons: power button pressed");
+            info!("hardware_buttons: power button pressed");
             message = Some(Message::ShowPowerOptions(true));
         }
         KeyEvent::Pressing(Key::Power) => {
-            println!("hardware_buttons: power button pressing");
+            info!("hardware_buttons: power button pressing");
             message = Some(Message::ShowPowerOptions(true));
         }
         KeyEvent::Unknown(Key::Power) => {
-            println!("hardware_buttons: power button unknown event");
+            info!("hardware_buttons: power button unknown event");
             message = Some(Message::ShowPowerOptions(true));
         }
         KeyEvent::Released(Key::Power) => {
-            println!("hardware_buttons: power button released");
+            info!("hardware_buttons: power button released");
         }
 
         KeyEvent::Pressed(Key::Home) => {
-            println!("hardware_buttons: home button pressed");
+            info!("hardware_buttons: home button pressed");
         }
         KeyEvent::Pressing(Key::Home) => {
-            println!("hardware_buttons: home button pressing");
+            info!("hardware_buttons: home button pressing");
         }
         KeyEvent::Released(Key::Home) => {
-            println!("hardware_buttons: home button released");
+            info!("hardware_buttons: home button released");
         }
         KeyEvent::Unknown(Key::Home) => {
-            println!("hardware_buttons: home button unknown event");
+            info!("hardware_buttons: home button unknown event");
         }
 
         KeyEvent::Pressed(Key::VolumeUp) => {
-            println!("hardware_buttons: volume up pressed");
+            info!("hardware_buttons: volume up pressed");
             message = Some(Message::VolumeUp);
         }
         KeyEvent::Pressing(Key::VolumeUp) => {
-            println!("hardware_buttons: volume up pressing");
+            info!("hardware_buttons: volume up pressing");
             message = Some(Message::VolumeUp);
         }
         KeyEvent::Released(Key::VolumeUp) => {
-            println!("hardware_buttons: volume up released");
+            info!("hardware_buttons: volume up released");
         }
         KeyEvent::Unknown(Key::VolumeUp) => {
-            println!("hardware_buttons: volume up unknown event");
+            info!("hardware_buttons: volume up unknown event");
             message = Some(Message::VolumeUp);
         }
 
         KeyEvent::Pressed(Key::VolumeDown) => {
-            println!("hardware_buttons: volume down pressed");
+            info!("hardware_buttons: volume down pressed");
             message = Some(Message::VolumeDown);
         }
         KeyEvent::Pressing(Key::VolumeDown) => {
-            println!("hardware_buttons: volume down pressing");
+            info!("hardware_buttons: volume down pressing");
             message = Some(Message::VolumeDown);
         }
         KeyEvent::Released(Key::VolumeDown) => {
-            println!("hardware_buttons: volume down released");
+            info!("hardware_buttons: volume down released");
         }
         KeyEvent::Unknown(Key::VolumeDown) => {
-            println!("hardware_buttons: volume down unknown event");
+            info!("hardware_buttons: volume down unknown event");
             message = Some(Message::VolumeDown);
         }
 
         KeyEvent::Pressed(Key::ExtensionDetection) => {
-            println!("hardware_buttons: extension detection pressed");
+            info!("hardware_buttons: extension detection pressed");
         }
         KeyEvent::Pressing(Key::ExtensionDetection) => {
-            println!("hardware_buttons: extension detection pressing");
+            info!("hardware_buttons: extension detection pressing");
         }
         KeyEvent::Released(Key::ExtensionDetection) => {
-            println!("hardware_buttons: extension detection released");
+            info!("hardware_buttons: extension detection released");
         }
         KeyEvent::Unknown(Key::ExtensionDetection) => {
-            println!("hardware_buttons: extension detection unknown event");
+            info!("hardware_buttons: extension detection unknown event");
         }
 
         KeyEvent::Pressed(Key::Unknown) => {
-            println!("hardware_buttons: unknown key pressed");
+            info!("hardware_buttons: unknown key pressed");
         }
         KeyEvent::Pressing(Key::Unknown) => {
-            println!("hardware_buttons: unknown key pressing");
+            info!("hardware_buttons: unknown key pressing");
         }
         KeyEvent::Released(Key::Unknown) => {
-            println!("hardware_buttons: unknown key released");
+            info!("hardware_buttons: unknown key released");
         }
         KeyEvent::Unknown(Key::Unknown) => {
-            println!("hardware_buttons: unknown key unknown event");
+            info!("hardware_buttons: unknown key unknown event");
         }
     }
 
