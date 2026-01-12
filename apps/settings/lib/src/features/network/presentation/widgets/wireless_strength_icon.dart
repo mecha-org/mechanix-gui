28c05bd60b2397e403db298cd3592352fab6aafb
ojas-mecha <ojasc@ext.mechasystems.com>
2026-01-12 12:55:31 +0530

    refactor(network): simplify wifi strength icon selection logic

--- apps/settings/lib/src/features/network/presentation/widgets/wireless_strength_icon.dart
+++ apps/settings/lib/src/features/network/presentation/widgets/wireless_strength_icon.dart
@@ -1,32 +1,48 @@
 import 'package:mechanix_settings/src/commons/constants.dart';
+import 'package:mechanix_settings/src/features/network/models/types.dart';
 import 'package:widgets/mechanix.dart';
 
-IconWidget getWirelessStrengthIcon(
-    {required int strength, bool isActive = false, required bool isSecure}) {
-  if (isSecure) {
-    if (strength > 75) {
-      return _getIconWidget(
-          iconPath: Images.wifiHighLocked, isActive: isActive);
-    } else if (strength > 50) {
-      return _getIconWidget(
-          iconPath: Images.wifiMediumLocked, isActive: isActive);
-    } else if (strength > 25) {
-      return _getIconWidget(iconPath: Images.wifiLowLocked, isActive: isActive);
-    } else {
-      return _getIconWidget(iconPath: Images.wifiNone, isActive: isActive);
-    }
-  } else {
-    if (strength > 75) {
-      return _getIconWidget(iconPath: Images.wifiHighOpen, isActive: isActive);
-    } else if (strength > 50) {
-      return _getIconWidget(
-          iconPath: Images.wifiMediumOpen, isActive: isActive);
-    } else if (strength > 25) {
-      return _getIconWidget(iconPath: Images.wifiLowOpen, isActive: isActive);
-    } else {
-      return _getIconWidget(iconPath: Images.wifiNone, isActive: isActive);
-    }
-  }
+IconWidget getWirelessStrengthIcon({
+  required int strength,
+  bool isActive = false,
+  required bool isSecure,
+}) {
+  final SignalLevel level = _getSignalLevel(strength);
+
+  return _getIconWidget(
+    iconPath: _getIconPath(level: level, isSecure: isSecure),
+    isActive: isActive,
+  );
+}
+
+SignalLevel _getSignalLevel(int strength) {
+  return switch (strength) {
+    > 75 => SignalLevel.high,
+    > 50 => SignalLevel.medium,
+    > 25 => SignalLevel.low,
+    _ => SignalLevel.none,
+  };
+}
+
+String _getIconPath({
+  required SignalLevel level,
+  required bool isSecure,
+}) {
+  const secureIcons = {
+    SignalLevel.high: Images.wifiHighLocked,
+    SignalLevel.medium: Images.wifiMediumLocked,
+    SignalLevel.low: Images.wifiLowLocked,
+    SignalLevel.none: Images.wifiNone,
+  };
+
+  const openIcons = {
+    SignalLevel.high: Images.wifiHighOpen,
+    SignalLevel.medium: Images.wifiMediumOpen,
+    SignalLevel.low: Images.wifiLowOpen,
+    SignalLevel.none: Images.wifiNone,
+  };
+
+  return isSecure ? secureIcons[level]! : openIcons[level]!;
 }
 
 IconWidget _getIconWidget({required String iconPath, required bool isActive}) {
