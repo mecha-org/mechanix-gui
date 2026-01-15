cf62ff67918158191749352d66d4fd7f948d9b10
swetar-mecha <swetar@mechasystems.com>
2026-01-15 11:10:48 +0530

    fix: icons parsing

--- shell/crates/icons/src/icons.rs
+++ shell/crates/icons/src/icons.rs
@@ -694,7 +694,7 @@ where
         };
     }
 
-    if !merged.as_array().iter().len() == 0 {
+    if merged.as_array().iter().len() >= 0 {
         merged.clone().try_into().unwrap_or_default()
     } else {
         T::default()
