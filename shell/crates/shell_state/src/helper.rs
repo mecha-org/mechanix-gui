0bfb410c234f5169bfaa7c69a76c2f32bdf4fa81
swetar-mecha <swetar@mechasystems.com>
2026-01-20 14:20:50 +0530

    refactor: get sink info for volume  data

--- shell/crates/shell_state/src/helper.rs
+++ shell/crates/shell_state/src/helper.rs
@@ -1,3 +1,4 @@
+use std::{thread, time::Duration};
 use crate::messages::ShellStateMessage;
 use bluez::service::BluetoothService;
 use chrono::Local;
@@ -90,6 +91,7 @@ pub async fn get_sound_device_info(
     tx: &mut mpsc::Sender<ShellStateMessage>,
     pulse_service: &PulseAudioService,
 ) {
+            let _ = thread::sleep(Duration::from_millis(5));
     if let Ok(device_info) = pulse_service.handle.get_default_sink().await {
         let _ = tx
             .send(ShellStateMessage::OutputSoundDevice { device_info })
--- shell/crates/shell_state/src/lib.rs
+++ shell/crates/shell_state/src/lib.rs
@@ -1,4 +1,3 @@
-use std::thread;
 use std::time::Duration;
 use bluez::{interfaces::device::BluetoothDevice, service::{BluetoothEvent, BluetoothService, InterfaceEvent}};
 use pulseaudio::service::{DeviceInfo, PulseAudioService};
@@ -464,7 +463,6 @@ impl ShellStateManager {
                                  Some(VolumeMessage::SetDefaultOutputSoundDevice { name }) => {
                                     match pulse_manager.handle.set_default_sink_by_name(&name).await {
                                         Ok(_) => {
-                                            let _ = thread::sleep(Duration::from_millis(5));
                                             let _= get_sound_device_info(&mut message_tx, &pulse_manager).await;
                                         }
                                         Err(e) => {
