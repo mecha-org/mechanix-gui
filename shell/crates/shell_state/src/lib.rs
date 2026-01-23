a819b075d5a04d1c211436f17a645c9132293153
swetar-mecha <swetar@mechasystems.com>
2026-01-23 18:12:28 +0530

    update(shell-state): remove wait when default sound device set

--- shell/crates/shell_state/src/lib.rs
+++ shell/crates/shell_state/src/lib.rs
@@ -1,4 +1,5 @@
-use std::{thread, time::Duration};
+use std::time::Duration;
+
 use bluez::{interfaces::device::BluetoothDevice, service::{BluetoothEvent, BluetoothService, InterfaceEvent}};
 use pulseaudio::service::{DeviceInfo, PulseAudioService};
 use futures::{FutureExt, SinkExt, StreamExt, channel::mpsc, select};
@@ -464,7 +465,6 @@ impl ShellStateManager {
                                  Some(VolumeMessage::SetDefaultOutputSoundDevice { name }) => {
                                     match pulse_manager.handle.set_default_sink_by_name(&name).await {
                                         Ok(_) => {
-                                            let _ = thread::sleep(Duration::from_millis(5));
                                             let _= get_sound_device_info(&mut message_tx, &pulse_manager).await;
                                         }
                                         Err(e) => {
