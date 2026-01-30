49dda593aeb0032f9113f2129952110a4e50e332
swetar-mecha <swetar@mechasystems.com>
2026-01-30 11:39:06 +0530

    fix: udpate icon size and spacing in status bar, lock screen

--- shell/crates/status_bar/src/ui/mod.rs
+++ shell/crates/status_bar/src/ui/mod.rs
@@ -6,6 +6,8 @@ use std::time::Duration;
 use theme::prelude::{AlphaExt, Fonts, Theme};
 use upower::interfaces::device::{BatteryLevel, BatteryState};
 
+pub const STATUS_ICON_GAP: f32 = 20.0;
+
 pub struct StatusBar {
     pub wireless_connected: bool,
     pub bluetooth_connected: bool,
@@ -258,11 +260,11 @@ pub fn status_bar_components(
         .child(
             div()
                 .flex()
-                .gap_4()
-                .child(img(wireless_icon_path).w(px(20.)).h(px(20.)))
+                .gap(px(STATUS_ICON_GAP))
+                .child(img(wireless_icon_path).w(px(24.)).h(px(24.)))
                 .when(bluetooth_enabled, |this_div| {
-                    this_div.child(img(bluetooth_icon_path).w(px(20.)).h(px(20.)))
+                    this_div.child(img(bluetooth_icon_path).w(px(24.)).h(px(24.)))
                 })
-                .child(img(battery_icon_path).w(px(20.)).h(px(20.))),
+                .child(img(battery_icon_path).w(px(24.)).h(px(24.))),
         )
 }
